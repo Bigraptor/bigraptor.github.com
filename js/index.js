64be35ad9d074e5e219fd6d10e76670c406c5652
Bigraptor <bigraptor@iseogjuui-MacBook-Pro.local>
2018-11-25 00:02:26 +0900

    메인페이지 레이아웃 완성

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(
+    function() {
+        $( '.scene1_header' ).fadeIn( 3000 );
+        $('#menu_1').delay(1500).fadeIn(2000);
+        $('#menu_2').delay(2500).fadeIn(2000);
+    }
+);
